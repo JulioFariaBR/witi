b4e21edd390fe61e76372f2ca01f43c977c347e6
Julio Faria <julio.cesar.faria2021@gmail.com>
2024-02-07 18:56:12 -0300

    0.0.1

--- js/script-a.js
+++ js/script-a.js
@@ -1,7 +1,9 @@
+dadosRecebidos = location.search.replace(/[^0-9]/g, '')
+
 async function request() {
     const res = await fetch("../data/imprensus.json")
     const data = await res.json();
-    console.log(data);
+    console.log(data[dadosRecebidos])
 }
 
 request()
