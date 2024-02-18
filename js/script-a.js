927fb0deed6ede7b12fdf0f6231eec4aabe76655
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-02-18 13:34:14 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -1,9 +1,25 @@
-dadosRecebidos = location.search.replace(/[^0-9]/g, '')
+const dadosRecebidos = location.search.replace(/[^0-9]/g, '')
+const titleh1 = document.querySelector('.cab__title--h1');
+const principalh1 = document.querySelector('.principal__h1');
+const principala = document.querySelector('.principal__sect--a');
 
-async function request() {
-    const res = await fetch("../data/imprensus.json")
-    const data = await res.json();
-    console.log(data[dadosRecebidos])
+
+function titulo(dados) {
+    document.title = `Convite de ${dados} - WITI - Who Is The Impostor`;
+    titleh1.innerHTML = `Convite de ${dados} - WITI - Who Is The Impostor`;
+    principalh1.innerHTML = `Você foi convidado por ${dados}, para participar do servidor do WITI - Who Is The Impostor, um servidor de eventos de Among Us Brasileiro, que está sendo realizado. Se você quiser apoiar o projeto do WITI basta entrar no servidor e participar dos nossos eventos realizados.`;
 }
 
-request()
+function links(dados) {
+    principala.href = dados;
+}
+
+async function request() {
+    const res = await fetch("../data/imprensus.json");
+    const data = await res.json();
+    dadosSelecionados = (data[dadosRecebidos]);
+    titulo(dadosSelecionados.name);
+    links(dadosSelecionados.link);
+}
+
+request();
